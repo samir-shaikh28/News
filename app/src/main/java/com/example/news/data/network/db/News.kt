119910e280a6a85b6e8f93b7c7f50e71b36eb048
Vishal Vyas <vishal@Pushans-MacBook-Pro.local>
2019-07-15 14:34:37 +0530

    room setting up

--- app/src/main/java/com/example/news/data/network/db/News.kt
+++ app/src/main/java/com/example/news/data/network/db/News.kt
@@ -1,10 +1,17 @@
 package com.example.news.data.network.db
 
-data class News (val author: String?,
-                 val title: String?,
-                 val content: String?,
-                 val description: String?,
-                 val publishedAt: String?,
-                 val urlToImage: String?,
-                 val url: String?,
-                 val source: Source?)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "news")
+data class News (
+    @PrimaryKey val _id: Int,
+    @ColumnInfo(name = "author") val author: String?,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "content") val content: String?,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "publishedAt") val publishedAt: String?,
+    @ColumnInfo(name = "urlToImage") val urlToImage: String?,
+    @ColumnInfo(name = "url") val url: String?,
+    @ColumnInfo(name = "source") val source: Source?)
--- app/src/main/java/com/example/news/data/network/db/NewsDao.kt
+++ app/src/main/java/com/example/news/data/network/db/NewsDao.kt
@@ -1,2 +1,18 @@
 package com.example.news.data.network.db
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+public interface NewsDao {
+
+    @Insert
+    fun insert(news: News)
+
+    @Update
+    fun update(news: News)
+
+
+    @Query("select * from news")
+    fun selectAllNews(): LiveData<List<News>>
+}
