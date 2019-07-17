4c5ae8cc9a7ce565ffcb4dc1b76c7c89f0473cda
Samir Shaikh <samir@tickledmedia.com>
2019-07-17 16:03:37 +0530

    Implemented Singleton pattern for retrofit instance

--- app/src/main/java/com/example/news/data/network/ApiClient.kt
+++ app/src/main/java/com/example/news/data/network/ApiClient.kt
@@ -1,21 +1,15 @@
 package com.example.news.data.network
 
+import android.util.Log
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ApiClient {
-    companion object {
-        val BASE_URL: String = "https://newsapi.org/"
-        var retrofit: Retrofit? = null
-
-        fun getApiClient(): Retrofit? {
-            if (retrofit == null) {
-                retrofit = Retrofit.Builder().baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
-            }
-            return retrofit
-        }
-    }
+object ApiClient {
+    private const val BASE_URL: String = "https://newsapi.org/"
+    var retrofit: Retrofit =
+        Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 }
+
 
